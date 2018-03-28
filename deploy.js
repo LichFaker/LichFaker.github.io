c987619d8635e18ce8381d10478ea65adc401b02
LichFaker <lichfaker@gmail.com>
2018-03-28 17:29:28 +0800

    Fix ftp无法创建文件的问题

--- deploy.js
+++ deploy.js
@@ -1,30 +1,67 @@
 #!/usr/bin/env node
 
-var fs = require('vinyl-fs')
-var ftp = require('vinyl-ftp')
-var minimist = require('minimist')
-var args = minimist(process.argv.slice(2))
+var glob = require("glob");
+var Client = require("ftp");
+var minimist = require("minimist");
+var args = minimist(process.argv.slice(2));
 
-const remoteFolder = '/htdocs/'
+const remoteFolder = "/htdocs/";
 
-var ftp_user, ftp_pass, ftp_host
-console.log(args)
+const PORT = 21;
+console.log(args);
 if (!args.ftp_user || !args.ftp_pass || !args.ftp_host) {
-    console.error("Unsupport ftp args!")
-    return
+  console.error("Unsupport ftp args!");
+  return;
 }
 
-var conn = ftp.create({
-    host: args.ftp_host,
-    user: args.ftp_user,
-    password: args.ftp_pass,
-    parallel: 10,
-})
-console.log("starting upload")
-let start = Date.now()
-fs.src(['./**/*.html', 'atom.xml', 'index.html'], {
-        buffer: false
-    })
-    .pipe(conn.differentSize(remoteFolder))
-    .pipe(conn.dest(remoteFolder))
-    
+var client = new Client();
+
+client.on("ready", function() {
+  client.cwd(remoteFolder, err => {
+    if (!err) {
+      upload();
+    } else {
+      client.end();
+      console.log(err);
+    }
+  });
+});
+
+client.connect({
+  host: args.ftp_host,
+  port: PORT,
+  user: args.ftp_user,
+  password: args.ftp_pass
+});
+
+console.log("starting upload");
+
+function upload() {
+  var totalNum = 0;
+  var uploadCount = 0;
+  glob("**/*.?(html|xml|jpeg|png)", (err, files) => {
+    if (!err) {
+      totalNum = files.length;
+      files.forEach(f => {
+        let dir = f.substring(0, f.lastIndexOf("/"));
+        client.mkdir(dir, true, err => {
+          client.put(f, f, err => {
+            uploadCount++
+            if (err) {
+              console.log(err);
+            }
+            if (uploadCount === totalNum) {
+                client.end()
+                console.log("finishing upload");
+            }
+          });
+        });
+
+        // console.log(f);
+      });
+    } else {
+      console.log(err);
+      client.end()
+    }
+  });
+}
